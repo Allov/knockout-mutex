d63ff51603fcd1463807069e57a53b5412fe68f9
Allov <frederik.charest@gmail.com>
2015-04-13 11:25:28 -0400

    licensing

--- src/knockout-mutex.js
+++ src/knockout-mutex.js
@@ -1,3 +1,6 @@
+// Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 //TODO: Déplacer vers un bower component stand-alone
 define(['knockout'],
     function(ko) {
